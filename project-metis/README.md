24d9a03f16e4785554d567985f4969ab4c56742a
DavidA346 <davidaispuro346@gmail.com>
2024-12-17 17:39:03 -0800

    Starter code and entire homepage

---
+++ project-metis/README.md
@@ -0,0 +1,2 @@
+# ProjectMetis178
+A group programming project for UCR CS178A Senior Design.
---
+++ project-metis/src/App.jsx
@@ -0,0 +1,14 @@
+import './components/Homepage'
+import Homepage from './components/Homepage'
+
+
+function App() {
+
+  return (
+    <>
+     <Homepage />
+    </>
+  )
+}
+
+export default App
